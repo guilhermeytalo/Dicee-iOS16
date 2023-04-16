815b97e4adb7b0fe999e447349d28a5e8142e1c3
Guilherme Ytalo <gydaytalo@gmail.com>
2023-04-15 23:46:51 -0300

    first commit

---
+++ Dicee-iOS16/Dicee-iOS16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dicee-iOS16
+//
+//  Created by Guilherme Ytalo on 15/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
