a85cfe3e647265b04ee6665868ade032623ba9fb
Guilherme Ytalo <gydaytalo@gmail.com>
2023-07-26 19:01:15 -0300

    fix: update for use normal emulator

--- Dicee-iOS16/Dicee-iOS16/ViewController.swift
+++ Dicee-iOS16/Dicee-iOS16/ViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
